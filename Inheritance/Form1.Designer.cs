4cfef6c5d0d95657f5d5a1115d86a9c9f815d22a
Manchu505 <19881829+Manchu505@users.noreply.github.com>
2023-04-14 11:02:10 -0500

    Add project files.

---
+++ Inheritance/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Inheritance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.style = new System.Windows.Forms.TextBox();
+            this.txtABV = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BJCP Style";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Beer ABV";
+            // 
+            // style
+            // 
+            this.style.Location = new System.Drawing.Point(139, 159);
+            this.style.Name = "style";
+            this.style.Size = new System.Drawing.Size(100, 20);
+            this.style.TabIndex = 2;
+            // 
+            // txtABV
+            // 
+            this.txtABV.Location = new System.Drawing.Point(139, 198);
+            this.txtABV.Name = "txtABV";
+            this.txtABV.Size = new System.Drawing.Size(100, 20);
+            this.txtABV.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Show Beer Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Beer Color (°L)";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(139, 241);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtABV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.style);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox style;
+        private System.Windows.Forms.TextBox txtABV;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Inheritance/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Inheritance
+{
+    public partial class Form1 : Form
+    {
+        class BJCP_Style
+        {
+            protected string BJCP;
+
+            public BJCP_Style(string style)
+            {
+                if (style.Length == 0)
+                {
+                    MessageBox.Show("Enter Valid Catagory!");
+                    BJCP = "00";
+                }
+                else
+                {
+                    BJCP = style;
+                    //MessageBox.Show("Inside BJCP_Style Constructor " + BJCP);
+                }
+            }
+            public void display()
+            {
+                MessageBox.Show("BJCP_Style " + BJCP + " Display");
+
+            }
+        }
+        class Beer_ABV : BJCP_Style
+        {
+            private double ABV;
+
+            public Beer_ABV(string style, double abv) : base(style)
+            {
+                if (abv < 6.00)
+                {
+                    //ABV = 6.00;
+                    MessageBox.Show("BJCP 21A should be above 6%");
+                }
+                else
+                {
+                    ABV = abv;
+                }
+            }
+            public void display()
+            {
+                MessageBox.Show("Beer Alcohol by Volume = " + ABV + "%");
+            }
+        }
+        class Beer_Color : BJCP_Style
+        {
+            private double SRM;
+
+            public Beer_Color(string style, double srm) : base(style)
+            {
+                if (srm == 0)
+                { 
+
+                }
+                else if (srm < 10)
+                {
+                    //SRM = 10;
+                    MessageBox.Show("BJCP 21A should be above 10 °L");
+                }
+                else
+                {
+                    SRM = srm;
+                }
+            }
+            public void display()
+            {
+                MessageBox.Show("Beer Color in °L = " + SRM + "°L");
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BJCP_Style b1 = new BJCP_Style(style.Text);
+            b1.display();
+            if (string.IsNullOrEmpty(txtABV.Text))
+                {
+
+                }
+            else
+            {
+                double txtboxABV = double.Parse(txtABV.Text);
+                Beer_ABV bn = new Beer_ABV(style.Text, txtboxABV);
+                bn.display();
+            }
+            if (string.IsNullOrEmpty(txtColor.Text))
+            {
+
+            }
+            else
+            {
+                double txtboxSRM = double.Parse(txtColor.Text);
+                Beer_Color bc = new Beer_Color(style.Text, txtboxSRM);
+                bc.display();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtABV.Clear();
+            txtColor.Clear();
+            style.Clear();
+        }
+    }
+}
